cb9f17b4b7523277e12ba177bad6d80902cbb0d6
andresguerrah <andres.guerrah@gmail.com>
2022-08-12 01:19:43 +0000

    Avance1

--- src/models.py
+++ src/models.py
@@ -29,5 +29,23 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+
+class Addresss(Base):
+    __tablename__ = 'addresss'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+
+
+    def to_dict(self):
+        return {}
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
